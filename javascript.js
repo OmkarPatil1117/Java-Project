ebea56f788fe70ca31fc250857e181d9e1f4a001
OmkarPatil1117 <omkrpatil117@gmail.com>
2022-11-07 13:16:31 +0530

    all functinality complited

--- javascript.js
+++ javascript.js
@@ -44,19 +44,89 @@ var setalarm = ()=> {
     var m = document.getElementById("timingMorning").value;
     localStorage.setItem("morning", m );
 
+    if(localStorage.getItem("morning") === "1") {
+        document.getElementById("demo1").innerHTML = "08 AM - 09 AM";
+    }
+
+    if(localStorage.getItem("morning") === "2") {
+        document.getElementById("demo1").innerHTML = "09 AM - 10 AM";
+    }
+    if(localStorage.getItem("morning") === "3") {
+        document.getElementById("demo1").innerHTML = "09 AM - 10 AM";
+    }
+    if(localStorage.getItem("morning") === "4") {
+        document.getElementById("demo1").innerHTML = "09 AM - 10 AM";
+    }
+
     var lauch = document.getElementById("afternoontiming").value;
     localStorage.setItem("afternoon", lauch );
 
+    if(localStorage.getItem("afternoon") === "1") {
+        document.getElementById("demo2").innerHTML = "12 PM - 01 PM";
+    }
+
+    if(localStorage.getItem("afternoon") === "2") {
+        document.getElementById("demo2").innerHTML = "01 PM - 02 PM";
+    }
+    if(localStorage.getItem("afternoon") === "3") {
+        document.getElementById("demo2").innerHTML = "02 PM - 03 PM";
+    }
+    if(localStorage.getItem("afternoon") === "4") {
+        document.getElementById("demo2").innerHTML = "03 PM - 04 PM";
+    }
+
     var evening = document.getElementById("evening").value;
     localStorage.setItem("evening", evening );
-    
+
+    if(localStorage.getItem("evening") === "1") {
+        document.getElementById("demo3").innerHTML = "04 PM - 05 PM";
+    }
+
+    if(localStorage.getItem("evening") === "2") {
+        document.getElementById("demo3").innerHTML = "05 PM - 06 PM";
+    }
+    if(localStorage.getItem("evening") === "3") {
+        document.getElementById("demo3").innerHTML = "06 PM - 07 PM";
+    }
+    if(localStorage.getItem("evening") === "4") {
+        document.getElementById("demo3").innerHTML = "07 PM - 08 PM";
+    }
+
     var night = document.getElementById("night").value;
     localStorage.setItem("night", night );
+
+    if(localStorage.getItem("night") === "1") {
+        document.getElementById("demo4").innerHTML = "08 PM - 09 PM";
+    }
+
+    if(localStorage.getItem("night") === "2") {
+        document.getElementById("demo4").innerHTML = "09 PM - 10 PM";
+    }
+    if(localStorage.getItem("night") === "3") {
+        document.getElementById("demo4").innerHTML = "10 PM - 11 PM";
+    }
+    if(localStorage.getItem("night") === "4") {
+        document.getElementById("demo4").innerHTML = "11 PM - 12 AM";
+    }
 }
+
+setTimeout(() => {
+    setalarm();
+}, 100);
+
 document.getElementById("timingMorning").value = localStorage.getItem("morning");
 document.getElementById("afternoontiming").value = localStorage.getItem("afternoon");
 document.getElementById("evening").value = localStorage.getItem("evening");
 document.getElementById("night").value = localStorage.getItem("night");
+
+if(localStorage.getItem("morning") == null) {
+    document.getElementById("timingMorning").value = 0;
+    document.getElementById("afternoontiming").value = 0;
+    document.getElementById("evening").value = 0;
+    document.getElementById("night").value = 0;
+}
+
+
 
 
 
